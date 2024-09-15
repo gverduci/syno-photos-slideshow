249018a484025749b1f1687a6cb22f7659a4f76f
gverduci <g.verduci@gmail.com>
2024-09-15 17:00:43 +0200

    fix: clear cached data for the home directory path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import login from "@/actions/synologyAuth";
 import { Photo as PhotoType, getPhotos } from "@/actions/photos.action";
 import { Photo } from "@/component/ui/photo";
+import { revalidatePath } from "next/cache";
 
 export default async function Home() {
   const auth = await login();
-  const  photos: PhotoType[] = await getPhotos(auth.synotoken, auth.sid);
+  const photos: PhotoType[] = await getPhotos(auth.synotoken, auth.sid);
   
+  // the next time the home is displayed the cache will be revalidated
+  revalidatePath('/');
+
   return (
     <Photo photos={photos} currentIndex={0} token={auth.synotoken} sid={auth.sid} />
   );
